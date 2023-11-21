fbe9fc3e0f2a782a9b88b64e5099f30a07a45cd6
AplanaVM <alexander.alyabev@develonica.ru>
2023-11-21 23:07:34 +0300

    mthds

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const Koa = require('koa');
 const { koaBody } = require('koa-body');
 
 const app = new Koa();
+
+const subscriptions = [];
 console.log('TYPE: '+typeof koaBody);
 
 app.use(koaBody({
@@ -10,11 +12,34 @@ app.use(koaBody({
 }));
 
 app.use((ctx, next) => {
+  if (ctx.request.method !== 'OPTIONS') {
+    next();
+    return;
+  };
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, POST, GET');
+  ctx.response.status = 204;
+
+});
+
+app.use((ctx, next) => {
   console.log('ctx.request.body:' + JSON.stringify(ctx.request.body));
 
   ctx.response.set('Access-Control-Allow-Origin', '*');
 
-  ctx.response.body = 'server response';
+  const { name, phone } = ctx.request.body;
+
+  if (subscriptions.some(sub => sub.phone === phone)) {
+    ctx.response.status = 400;
+
+    ctx.response.body = 'subscription exists';
+
+    return;
+  }
+  subscriptions.push({ name, phone});
+
+  ctx.response.body = 'OK';
   next();
 });
 
