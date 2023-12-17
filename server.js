5940e889646bfc54774d805d15832eaddbbbea74
TheFortnight <aliabyev@gmail.com>
2023-12-18 01:12:28 +0300

    image undone

--- server.js
+++ server.js
@@ -1,27 +1,138 @@
 const http = require('http');
 const Koa = require('koa');
 const { koaBody } = require('koa-body');
-
 const app = new Koa();
+const path = require('path');
+const fs = require('fs');
+const uuid = require('uuid');
+
+let subscriptions = [];
 console.log('TYPE: '+typeof koaBody);
 
 app.use(koaBody({
   urlencoded: true,
+  multipart: true
 }));
 
 app.use((ctx, next) => {
-  console.log('ctx.request.body:' + JSON.stringify(ctx.request.body));
+  
+  if (ctx.request.method !== 'OPTIONS') {
+    next();
+    return;
+  };
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+  ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, POST, GET');
+  ctx.response.status = 204;});
+
+  app.use((ctx, next) => {
+
+    if (ctx.request.method !== 'POST' && ctx.request.url !== '/upload') {
+  
+      next();  
+      return;
+    }
+  
+    ctx.response.set('Access-Control-Allow-Origin', '*');
+    ctx.response.set('Access-Control-Allow-Methods', 'DELETE, PUT, PATCH, POST, GET');
+    
+    let fileName;
+
+    try {
+    const public = path.join(__dirname, '/public');
+
+    
+
+    const { file } = ctx.request.files;
+
+    const subfolder = uuid.v4();
+
+    const uploadFolder = public + '/' + subfolder;
+
+    //console.log('FILE PATH:', file.filepath)
+
+    fs.mkdirSync(uploadFolder);
+
+    fs.copyFileSync(file.filepath, uploadFolder + '/' + file.originalFilename);
+
+    fileName = '/public/' + subfolder + '/' + file.originalFilename;
+    
+    ctx.response.body = fileName;
+
+    } catch (error) {
+      console.log('ERROR: ', error);
+      ctx.response.status = 500;
+
+      return;
+    }
+
+    
+  
+    next();
+  
+  });
+
+
+/*app.use((ctx, next) => {
+
+  if (ctx.request.method !== 'POST') {
+
+    console.log('NOT POST REQ!');
+
+    next();
+
+    return;
+  }
+
+  //console.log('POST: ', ctx.request.body);
 
   ctx.response.set('Access-Control-Allow-Origin', '*');
 
-  ctx.response.body = 'server response';
+  let { name, phone } = ctx.request.body;
+
+  if (subscriptions.some(sub => sub.phone === phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = 'subscription exists';
+
+    return;
+  }
+  subscriptions.push({ name, phone});
+
+  ctx.response.body = 'KOK';
+
   next();
-});
+
+});*/
 
 app.use((ctx, next) => {
-  console.log('2nd middlewear');
-  
-  //next();
+
+  if (ctx.request.method !== 'DELETE') {
+    console.log('NOT DELETE REQ!');
+    next();
+
+    return;
+  }
+
+  console.log('DELETE: ', ctx.request.query);
+
+  ctx.response.set('Access-Control-Allow-Origin', '*');
+
+  let { phone } = ctx.request.query;
+
+  if (subscriptions.every(sub => sub.phone !== phone)) {
+    ctx.response.status = 400;
+    ctx.response.body = 'subscription doesn\'t exists';
+
+    return;
+  }
+
+
+  subscriptions = subscriptions.filter(sub => sub.phone !== phone);
+
+  ctx.response.body = 'Subscription deleted';
+
+  next();
+
 });
 
 const server = http.createServer(app.callback());
